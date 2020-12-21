a23d3c25a37353b5f709a4ac7e4ce2977125170e
Filip Godlewski <filip.godlewski@outlook.com>
2020-12-21 22:39:44 +0100

    configure setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,24 @@
-import setuptools
+import os
+from setuptools import setup, find_packages
 
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-if __name__ == "__main__":
-    setuptools.setup()
+
+setup(
+    name="pytomata",
+    version="0.2.0",
+    author="Filip Godlewski",
+    author_email="filip.godlewski@outlook.com",
+    description="Automated virtual environment management made dead-simple.",
+    long_description=read("README.md"),
+    license="MIT",
+    keywords="virtualenv venv",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["Click"],
+    entry_points={"console_scripts": ["pytomata=src.pytomata:cli"]},
+    classifiers=["Topic :: Software Development :: Build Tools"],
+)
---
+++ src/pytomata.py
@@ -0,0 +1,31 @@
+import click
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+def activate():
+    pass
+
+
+@cli.command()
+def remove():
+    pass
+
+
+@cli.command()
+def list():
+    pass
+
+
+@cli.command()
+def make():
+    pass
+
+
+@cli.command()
+def update():
+    pass
